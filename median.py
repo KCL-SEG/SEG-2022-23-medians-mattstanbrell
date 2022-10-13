feb1af6e447a86bd0545bb5a88106e6e13a23b7c
Matt Stanbrell <mattstanbrell@gmail.com>
2022-10-13 12:47:08 +0100

    Set result to print instead of just return

--- median.py
+++ median.py
@@ -14,7 +14,7 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
-        get_median(numbers)
+        print(get_median(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
