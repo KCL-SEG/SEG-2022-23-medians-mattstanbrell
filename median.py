314bc14cf7a2c2d78edc660657fd58b28168b6f9
Matt Stanbrell <mattstanbrell@gmail.com>
2022-10-13 12:39:13 +0100

    Working median finder

--- median.py
+++ median.py
@@ -1,12 +1,21 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+import math
+
+
+def get_median(numbers):
+    numbers.sort()
+    if len(numbers) % 2 == 0:
+        return (numbers[int(len(numbers) / 2)] + numbers[int((len(numbers) / 2) - 1)]) / 2
+    else:
+        return numbers[math.floor((len(numbers) / 2))]
+
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        get_median(numbers)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
